4f1de4f708461b56290f0b6f9ad1b8f6bbb55010
Mu-Solomon <musolomoni@gmail.com>
2024-02-07 23:28:21 +0300

    Google analytics now works!

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,14 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/images/favicon-32x32.png" />
+      <head>
+        <GoogleAnalytics gaId="G-GG1K2PTQXG" />
+        <link rel="icon" href="/images/favicon-32x32.png" />
+      </head>
       <body className="bg-[#1f2632] min-h-screen flex flex-col items-center justify-center w-screen">
         {children}{" "}
         <h2 className="text-[#52ffa8] tracking-[5px] text-xs font-extrabold absolute bottom-2">
           SOLOMON MUYONJO
         </h2>
       </body>
-      <GoogleAnalytics gaId="G-GG1K2PTQXG" />
     </html>
   );
 }
