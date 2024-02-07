7ce83521f5412da1afa052ffa8c10cdf656e5362
Mu-Solomon <musolomoni@gmail.com>
2024-02-07 20:43:51 +0300

    Added my name on the bottom

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,9 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <link rel="icon" href="/images/favicon-32x32.png" />
+      <link rel="icon" href="/images/favicon-32x32.png" />
       <body className="bg-[#1f2632] min-h-screen flex flex-col items-center justify-center w-screen">
-        {children}
+        {children}{" "}
+        <h2 className="text-[#52ffa8] tracking-[5px] text-xs font-extrabold absolute bottom-5">
+          SOLOMON MUYONJO
+        </h2>
       </body>
     </html>
   );
