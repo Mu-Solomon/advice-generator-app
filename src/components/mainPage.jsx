dc63e66b01f3acce118e731d4dba84fc44ec7ee8
Mu-Solomon <musolomoni@gmail.com>
2024-02-07 19:43:41 +0300

    Done with logic

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import { revalidatePath } from "next/cache";
+import { Suspense } from "react";
 
-const MainPage = () => {
+const getAdvice = async () => {
+  "use server";
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice", {
+      cache: "no-store",
+    });
+    revalidatePath("/");
+    return res.json();
+  } catch (error) {
+    throw new Error("Failed to fetch advice.", error);
+  }
+};
+
+const MainPage = async () => {
+  const advice = await getAdvice();
+
+  console.log(advice);
+  const text =
+    "“When having a clear out, ask yourself if an item has any financial, practical or sentimental value. If not, chuck it.”";
   return (
     <div
       className="flex flex-col items-center justify-center gap-5 bg-[#323a49] w-11/12 rounded-lg h-[370px] sm:h-[300px] sm:py-20 pt-16 pb-10 sm:w-[500px]"
@@ -8,14 +27,19 @@ const MainPage = () => {
     >
       <div className="mt-12 sm:mt-20" id="head">
         <h2 className="text-[#52ffa8] tracking-[5px] text-xs font-extrabold">
-          ADVICE #117
+          ADVICE #{advice?.slip.id}
         </h2>
       </div>
       <div id="body">
-        <p className="text-[#cee3e9] text-[28px] font-extrabold text-center px-6 md:px-10 w-full h-[200px] sm:h-[100px] md:leading-8 ">
-          “It is easy to sit up and take notice, what's difficult is getting up
-          and taking action.”
-        </p>
+        {advice?.slip.advice.length >= 88 ? (
+          <p className="text-[#cee3e9]  text-[24px] font-extrabold text-center px-6 md:px-10 w-full h-[200px] sm:h-[100px] md:leading-8 ">
+            “{advice?.slip.advice}”
+          </p>
+        ) : (
+          <p className="text-[#cee3e9]  text-[28px] font-extrabold text-center px-6 md:px-10 w-full h-[200px] sm:h-[100px] md:leading-8 ">
+            “ {advice?.slip.advice}”
+          </p>
+        )}
       </div>
       <div className="sm:my-3 " id="image">
         <img
@@ -23,12 +47,16 @@ const MainPage = () => {
           className="sm:w-[400px]"
         />
       </div>
-      <div
-        className="bg-[#52ffa8] p-5 rounded-full hover:cursor-pointer "
-        id="dice"
-      >
-        <img src="/images/icon-dice.svg" className="" />
-      </div>
+      <form action={getAdvice}>
+        <button>
+          <div
+            className="bg-[#52ffa8] p-5 rounded-full hover:cursor-pointer "
+            id="dice"
+          >
+            <img src="/images/icon-dice.svg" className="" />
+          </div>
+        </button>
+      </form>
     </div>
   );
 };
